71fd6c4b8f532b711ffb4d99e6823ee7d6d75783
Thomas Kluyver <takowl@gmail.com>
2015-07-13 20:58:40 -0500

    Initial commit

---
+++ recombinecm.py
@@ -0,0 +1,63 @@
+from copy import deepcopy
+import difflib
+from notebook.services.contents.filemanager import FileContentsManager
+
+def clean_copy(nb):
+    nb2 = deepcopy(nb)
+
+    for cell in nb2.cells:
+        if cell.cell_type == 'code':
+            cell.outputs = []
+            cell.execution_count = None
+
+    return nb2
+
+def junkfunc(source):
+    # Ignore empty cells for the sequence matching
+    return not source.strip()
+
+def recombine(clean, with_outputs):
+    clean_code_cells, clean_code_ixs = [], []
+    for i, cell in enumerate(clean.cells):
+        if cell.cell_type == 'code':
+            clean_code_cells.append(cell.source)
+            clean_code_ixs.append(i)
+
+    dirty_code_cells, dirty_code_ixs = [], []
+    for i, cell in enumerate(with_outputs.cells):
+        if cell.cell_type == 'code':
+            dirty_code_cells.append(cell.source)
+            dirty_code_ixs.append(i)
+
+    result = deepcopy(clean)
+
+    sm = difflib.SequenceMatcher(junkfunc, clean_code_cells, dirty_code_cells)
+
+    for match in sm.get_matching_blocks():
+        for i in range(match.size):
+            target_cell = result.cells[clean_code_ixs[match.a + i]]
+            source_cell = with_outputs.cells[dirty_code_ixs[match.b + i]]
+            target_cell.outputs = source_cell.outputs
+            target_cell.execution_count = source_cell.execution_count
+
+    return result
+
+class RecombineContentsManager(FileContentsManager):
+
+    def _save_notebook(self, os_path, nb):
+        super()._save_notebook(os_path, nb)
+
+        # Create another file with the clean copy of the notebook
+        nb2 = clean_copy(nb)
+
+        super()._save_notebook(os_path + '.clean', nb2)
+
+    def _read_notebook(self, os_path, as_version=4):
+        with_output = super()._read_notebook(os_path, as_version)
+        try:
+            clean = super()._read_notebook(os_path + '.clean', as_version)
+        except FileNotFoundError:
+            # We didn't save this file, so just return the regular notebook
+            return with_output
+
+        return recombine(clean, with_output)
---
+++ test_recombine.py
@@ -0,0 +1,165 @@
+from nbformat import from_dict
+from nbformat.v4 import new_notebook, new_code_cell, new_output, new_markdown_cell
+
+import recombinecm
+
+w_output_1 = new_notebook(cells = [
+    new_code_cell("print('foo')",
+                  execution_count=1,
+                  outputs = [
+                      new_output('stream', text='foo\n'),
+                  ]),
+    new_markdown_cell('This is some *markdown*'),
+    new_code_cell("a=1\na",
+                  execution_count=2,
+                  outputs = [
+                      new_output('execute_result',
+                                 {'text/plain': '1'},
+                                 execution_count=1)
+                  ]),
+    new_code_cell("print('fob')",
+                  execution_count=4,
+                  outputs = [
+                      new_output('stream', text='fob\n'),
+                  ]),
+])
+
+clean_1 = new_notebook(cells = [
+    new_code_cell("print('foo')"),
+    new_markdown_cell('This is some *markdown*'),
+    new_code_cell("a=1\na"),
+    new_code_cell("print('fob')"),
+])
+
+w_output_1_extra_cells = new_notebook(cells = [
+    new_code_cell("print('foo')",
+                  execution_count=1,
+                  outputs = [
+                      new_output('stream', text='foo\n'),
+                  ]),
+    new_markdown_cell('This is some *markdown*'),
+    new_code_cell("sys.stdout.write('qrx')",
+                  execution_count=3,
+                  outputs = [
+                      new_output('stream', text='qrx')
+                  ]),
+    new_markdown_cell('More text that should be ignored'),
+    new_code_cell("a=1\na",
+                  execution_count=2,
+                  outputs = [
+                      new_output('execute_result',
+                                 {'text/plain': '1'},
+                                 execution_count=1)
+                  ]),
+    new_code_cell("print('fob')",
+                  execution_count=4,
+                  outputs = [
+                      new_output('stream', text='fob\n'),
+                  ]),
+])
+
+clean_1_extra_cells = new_notebook(cells = [
+    new_code_cell("print('foo')"),
+    new_markdown_cell('This is some *markdown*'),
+    new_code_cell("sys.stdout.write('qrx')"),
+    new_markdown_cell('More text that should be ignored'),
+    new_code_cell("a=1\na"),
+    new_code_cell("print('fob')"),
+])
+
+clean_1_cells_removed = new_notebook(cells = [
+    new_markdown_cell('This is some *markdown*'),
+    new_code_cell("print('fob')"),
+])
+
+def test_clean_basic():
+    res = recombinecm.clean_copy(w_output_1)
+
+    c = res.cells
+    assert c[0].cell_type == 'code'
+    assert c[0].outputs == []
+    assert c[0].execution_count == None
+
+    assert c[1].cell_type == 'markdown'
+
+    assert c[2].cell_type == 'code'
+    assert c[2].outputs == []
+    assert c[2].execution_count == None
+
+    # Check that we didn't modify the original
+    assert w_output_1.cells[0].outputs != []
+
+def test_recombine_basic():
+    res = recombinecm.recombine(clean_1, w_output_1)
+
+    c = res.cells
+    assert c[0].cell_type == 'code'
+    assert len(c[0].outputs)
+    assert c[0].outputs[0].output_type == 'stream'
+    assert c[0].execution_count == 1
+
+    assert c[1].cell_type == 'markdown'
+
+    assert c[2].cell_type == 'code'
+    assert len(c[2].outputs) == 1
+    assert c[2].outputs[0].output_type == 'execute_result'
+    assert c[2].execution_count == 2
+
+    assert c[3].cell_type == 'code'
+    assert c[3].outputs[0].text == 'fob\n'
+
+    # Check that this is a separate object
+    assert res is not w_output_1
+    assert res is not clean_1
+    assert clean_1.cells[0].outputs == []
+
+def test_recombine_extra_locally():
+    res = recombinecm.recombine(clean_1, w_output_1_extra_cells)
+
+    c = res.cells
+    assert c[0].cell_type == 'code'
+    assert len(c[0].outputs)
+    assert c[0].outputs[0].output_type == 'stream'
+    assert c[0].execution_count == 1
+
+    assert c[1].cell_type == 'markdown'
+
+    assert c[2].cell_type == 'code'
+    assert len(c[2].outputs) == 1
+    assert c[2].outputs[0].output_type == 'execute_result'
+    assert c[2].execution_count == 2
+
+    assert c[3].cell_type == 'code'
+    assert c[3].outputs[0].text == 'fob\n'
+
+def test_recombine_extra_in_clean():
+    res = recombinecm.recombine(clean_1_extra_cells, w_output_1)
+
+    c = res.cells
+    assert c[0].cell_type == 'code'
+    assert len(c[0].outputs)
+    assert c[0].outputs[0].output_type == 'stream'
+    assert c[0].execution_count == 1
+
+    assert c[1].cell_type == 'markdown'
+
+    assert c[4].cell_type == 'code'
+    assert len(c[4].outputs) == 1
+    assert c[4].outputs[0].output_type == 'execute_result'
+    assert c[4].execution_count == 2
+
+    assert c[5].cell_type == 'code'
+    assert c[5].outputs[0].text == 'fob\n'
+
+    # This shouldn't match anything in the local version
+    assert c[2].cell_type == 'code'
+    assert c[2].outputs == []
+
+def test_recombine_clean_reduced():
+    res = recombinecm.recombine(clean_1_cells_removed, w_output_1)
+    c = res.cells
+
+    assert c[0].cell_type == 'markdown'
+
+    assert c[1].cell_type == 'code'
+    assert c[1].outputs[0].text == 'fob\n'
